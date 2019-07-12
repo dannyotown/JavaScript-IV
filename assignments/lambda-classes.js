29513b500d72be028b30627b6b07c5872b8eecf1
Danny Town <47186976+dannyotown@users.noreply.github.com>
2019-07-11 19:01:34 -0500

    Update!

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,45 @@
 // CODE here for your Lambda Classes
+class Person{
+    constructor(attributes){
+        this.age = attributes.age;
+        this.name = attributes.name;
+        this.location = attributes.location;
+    }
+    speak(){
+        return `Hello my name is ${this.name}, I am from ${this.location}`;
+    }
+}
+
+class Instructor extends Person{
+    constructor(attributes){
+        super(attributes)
+        this.specialty = attributes.specialty;
+        this.favLanguage = attributes.favLanguage;
+        this.catchPhrase = attributes.catchPhrase;
+    }
+    demo(){
+        return `Today we are learning about ${this.specialty}`;
+    }
+    grade(){
+        return `${this.name} recieves a perfect score on ${this.specialty}`;
+    }
+}
+
+class Student extends Person{
+    constructor(attributes){
+        super(attributes)
+        this.previousBackground = attributes.previousBackground;
+        this.className = attributes.className;
+        this.favSubjects = attributes.favSubjects;
+    }
+    listSubjects(){
+        let subArray = (this.favSubjects).map(sub => {return sub.favSubjects;});
+        console.log(subArray);
+    }
+    PRAssignment(arg){
+        console.log(`${this.name} has submitted a PR for ${arg}`);
+    }
+    sprintChallenge(arg){
+        console.log(`${this.name} has begun sprint challenge on ${arg}`);
+    }
+}
